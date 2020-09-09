1fd1110b6b7c5167346ba93d2c203f4c89f0901f
sunshine2285 <sunshine2285@163.com>
2020-09-09 21:53:15 +0800

    添加swagger注解

--- forum/src/main/java/com/ouc/forum/controller/FavoriteController.java
+++ forum/src/main/java/com/ouc/forum/controller/FavoriteController.java
@@ -3,6 +3,8 @@ package com.ouc.forum.controller;
 import com.ouc.forum.DTO.TestDTO;
 import com.ouc.forum.entity.Favorite;
 import com.ouc.forum.service.FavoriteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/favorite")
+@Api(value = "用户收藏接口", tags = "123")
 public class FavoriteController {
 
     final FavoriteService favoriteService;
@@ -23,23 +26,19 @@ public class FavoriteController {
         this.favoriteService = favoriteService;
     }
 
+    @ApiOperation(value = "获取用户收藏", notes = "根据用户ID获取用户所有收藏")
     @GetMapping("/{uid}")
     public ArrayList<Favorite> get(@PathVariable long uid) {
         return favoriteService.selectAll(uid);
     }
 
-    @PostMapping("/{uid}")
-    public String test(@PathVariable long uid,TestDTO testDTO) {
-        System.out.println(uid);
-        System.out.println(testDTO);
-        return testDTO.toString();
-    }
-
+    @ApiOperation(value = "删除用户收藏", notes = "根据用户ID和帖子ID删除用户收藏")
     @DeleteMapping("/{uid}/{tid}")
     public void delete(@PathVariable long uid, @PathVariable long tid) {
         favoriteService.delete(uid, tid);
     }
 
+    @ApiOperation(value = "添加用户收藏", notes = "根据用户ID和帖子ID为用户添加收藏")
     @PostMapping("/{uid}/{tid}")
     public Favorite add(@PathVariable long uid, @PathVariable long tid) {
         Favorite favorite = new Favorite();
